8b2f986e8555c6c432a0e42941734c61dc9b2768
maurineA <maurinawambuia@gmail.com>
2023-12-31 13:47:04 +0300

    returns the restaurant instance that has the highest star rating from this customer

--- lib/customer.py
+++ lib/customer.py
@@ -17,3 +17,10 @@ class Customer(Base):
     # returns the full name of the customer, with the first name and the last name  concatenated
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+    
+    #returns the restaurant instance that has the highest star rating from this customer
+    def favorite_restaurant(self, session: Session):
+        sorted_reviews = sorted(self.reviews, key=lambda x: x.star_rating, reverse=True)
+        if sorted_reviews:
+            return sorted_reviews[0].restaurant
+        return None

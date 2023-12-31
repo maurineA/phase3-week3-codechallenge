0148ec3f62edc6b370b20bb5c85cceeff2d8061c
maurineA <maurinawambuia@gmail.com>
2023-12-31 15:16:33 +0300

    added the readme file

---
+++ README.md
@@ -0,0 +1,62 @@
+# PHASE3-WEEK2-CODECHALLENGE
+
+Is a python challenge based on SQLALchemy working with a restaurant review domain checking the object relation of  restaurants, customers and reviews. DECEMBER 31/2023
+
+By Maurine Wambui
+
+# Description
+
+Is a python challenge based on SQLALchemy working with a restaurant review domain checking the SQLAlchemy Migrations,  SQLAlchemy Relationships, Class and Instance Methods, SQLAlchemy Querying of  restaurants, customers and reviews. `Restaurant` has many `Review`s, a `Customer` has many `Review`s, and a `Review` belongs to a `Restaurant` and to a `Customer`. `Restaurant` - `Customer` is a many to many relationship.
+
+# How to Use
+
+<ul>
+<li>Test the code in the python environment shell
+      <li>python/app.py  #my test are not passing i'm having errors in folders in the orm, still working on it</li>
+      
+</li>
+
+</ul>
+
+# Requirements
+
+<ul>
+<li>A computer with python environment, pipenv and pipenvshell</li>
+<li>Access to the internet.</li>
+</ul>
+
+# Installation Process
+
+<ul>
+<li>git clone https://github.com/maurineA/phase3-week3-codechallenge</li>
+<li>open the directory on visual studio</li>
+<li>Create the python environment</li>
+<li>install the pipenv</li>
+<li>activate the shell</li>
+</ul>
+
+# Technologies Used
+<ul>
+<li>Python</li>
+</ul>
+
+
+# Support and Contact Details
+
+Incase of any queries, need for collaboration or issues with this code, feel free to reach me at maurine.wambui@student.moringaschool.com
+
+# License
+
+MIT License
+
+Copyright © 2023 Maurine Wambui 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+  
--- app.py
+++ app.py
@@ -1,40 +1,68 @@
+# from sqlalchemy.orm import Session
+# from sqlalchemy import create_engine
+# from lib.customer import Customer
+# from lib.restaurant import Restaurant
+# from lib.review import Review
+
+# DATABASE_URL = "sqlite:///restaurant_review.db"
+# engine = create_engine(DATABASE_URL)
+# session = Session(engine)
+
+# # Create instances
+# restaurant1 = Restaurant(name="Hotpoint", price=300)
+# restaurant2 = Restaurant(name="Yummies", price=500)
+
+# customer1 = Customer(first_name="Elisha", last_name="Mwangi")
+# customer2 = Customer(first_name="Sophia", last_name="Njeri")
+
+# # Add reviews
+# customer1.add_review(restaurant1, 5, session)
+# customer2.add_review(restaurant2, 4, session)
+# customer1.add_review(restaurant1, 3, session)
+
+# # customer details
+# print(customer1.full_name())
+# print([review.full_review() for review in customer1.reviews])
+# print([restaurant.name for restaurant in customer1.restaurants])
+# favorite_restaurant = customer1.favorite_restaurant(session)
+# print(favorite_restaurant.name if favorite_restaurant else "No favorite restaurant")
+
+# # restaurant information
+# print([review.full_review() for review in restaurant1.reviews(session)])
+# print([customer.full_name() for customer in restaurant1.customers(session)])
+# print(restaurant1.all_reviews(session))
+# fanciest_restaurant = Restaurant.fanciest(session)
+# print(fanciest_restaurant.name if fanciest_restaurant else "No fanciest restaurant")
+
+# # review details
+# print([review.full_review() for review in Review.all(session)])
+
+# session.close()
+
+# app.js
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from lib.customer import Customer
 from lib.restaurant import Restaurant
-from lib.review import Review
 
 DATABASE_URL = "sqlite:///restaurant_review.db"
 engine = create_engine(DATABASE_URL)
 session = Session(engine)
 
 # Create instances
-restaurant1 = Restaurant(name="Hotpoint", price=300)
-restaurant2 = Restaurant(name="Yummies", price=500)
+restaurant = Restaurant(name="Tasty Burgers", price=400)
+customer = Customer(first_name="John", last_name="Doe")
 
-customer1 = Customer(first_name="Elisha", last_name="Mwangi")
-customer2 = Customer(first_name="Sophia", last_name="Njeri")
+# Add a review
+customer.add_review(restaurant, 5, session)
 
-# Add reviews
-customer1.add_review(restaurant1, 5, session)
-customer2.add_review(restaurant2, 4, session)
-customer1.add_review(restaurant1, 3, session)
+# Print customer output
+print(customer.full_name())
+print([review.full_review() for review in customer.reviews])
 
-# customer details
-print(customer1.full_name())
-print([review.full_review() for review in customer1.reviews])
-print([restaurant.name for restaurant in customer1.restaurants])
-favorite_restaurant = customer1.favorite_restaurant(session)
-print(favorite_restaurant.name if favorite_restaurant else "No favorite restaurant")
-
-# restaurant information
-print([review.full_review() for review in restaurant1.reviews(session)])
-print([customer.full_name() for customer in restaurant1.customers(session)])
-print(restaurant1.all_reviews(session))
-fanciest_restaurant = Restaurant.fanciest(session)
-print(fanciest_restaurant.name if fanciest_restaurant else "No fanciest restaurant")
-
-# review details
-print([review.full_review() for review in Review.all(session)])
+# Print restaurant information
+print([review.full_review() for review in restaurant.reviews(session)])
+print([customer.full_name() for customer in restaurant.customers(session)])
 
 session.close()
+

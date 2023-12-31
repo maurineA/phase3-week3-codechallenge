86dc1c77e1e3e68e32d8b92f7bb0340477f2d8bf
maurineA <maurinawambuia@gmail.com>
2023-12-31 14:12:08 +0300

    tests

--- app.py
+++ app.py
@@ -8,16 +8,31 @@ DATABASE_URL = "sqlite:///restaurant_review.db"
 engine = create_engine(DATABASE_URL)
 session = Session(engine)
 
-# Assuming you have a session object
-session = Session()
+# Create instances
+restaurant1 = Restaurant(name="Hotpoint", price=3)
+restaurant2 = Restaurant(name="Yummies", price=4)
 
-# Create instances of Restaurant and Customer
-restaurant = Restaurant(name='Sample Restaurant', price=3)
-customer = Customer(first_name='John', last_name='Doe')
+customer1 = Customer(first_name="John", last_name="Doe")
+customer2 = Customer(first_name="Jane", last_name="Doe")
 
-# Create a review and associate it with the restaurant and customer
-review = Review(star_rating=4, restaurant=restaurant, customer=customer)
+# Add reviews
+customer1.add_review(restaurant1, 5, session)
+customer1.add_review(restaurant2, 4, session)
+customer2.add_review(restaurant1, 3, session)
 
-# Add the instances to the session and commit
-session.add_all([restaurant, customer, review])
-session.commit()
+# Print customer output
+print(customer1.full_name())
+print(customer1.reviews(session))
+print(customer1.restaurants(session))
+print(customer1.favorite_restaurant(session).name)
+
+# Print restaurant information
+print(restaurant1.reviews(session))
+print(restaurant1.customers(session))
+print(restaurant1.all_reviews(session))
+print(Restaurant.fanciest(session).name)
+
+# Print review information
+print(Review.all_reviews(session))
+
+session.close()

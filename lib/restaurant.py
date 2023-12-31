9507c813f4185b1d9e6cf72a26ffe734d04276d6
maurineA <maurinawambuia@gmail.com>
2023-12-31 13:57:11 +0300

    returns _one_ restaurant instance for the restaurant that has the highest price

--- lib/restaurant.py
+++ lib/restaurant.py
@@ -13,3 +13,13 @@ class Restaurant(Base):
     #relations
     reviews = relationship("Review", back_populates="customer")
     restaurants = relationship("Restaurant", secondary="reviews", back_populates="customers")
+
+    # returns _one_ restaurant instance for the restaurant that has the highest price
+    @classmethod
+    def fanciest(cls, session: Session):
+        return (
+            session.query(cls)
+            .order_by(cls.price.desc())
+            .limit(1)
+            .first()
+        )

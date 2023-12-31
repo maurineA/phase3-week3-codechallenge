43423a84777c6cbcf96a66f856d4efd1e477168e
maurineA <maurinawambuia@gmail.com>
2023-12-31 13:43:25 +0300

    define the relationship of customer with other tables

--- lib/customer.py
+++ lib/customer.py
@@ -5,7 +5,11 @@ Base = declarative_base()
 
 class Customer(Base):
     __tablename__ = "customers"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String)
     last_name = Column(String)
+
+    #relations
+    reviews = relationship("Review", back_populates="customer")
+    restaurants = relationship("Restaurant", secondary="reviews", back_populates="customers")
